5920347a05dbc5c97b860710ec3b36df46d2e373
Simao-Soares <44780464+Simao-Soares@users.noreply.github.com>
2021-07-15 17:56:47 +0100

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public  PlayerPaddle paddle;
+    public Ball ball;
+    private int _playerScore;
+    public Text playerScoreText;
+
+    public int _scoreLimit;
+
+    private int _computerScore;
+    public Text computerScoreText;
+
+    public void PlayerScores(){
+        _playerScore++;
+
+        Debug.Log(_playerScore);
+
+        if(_playerScore == _scoreLimit){
+            this.paddle.Shrink();
+            this.ScoreReset();
+        }
+
+        this.playerScoreText.text = _playerScore.ToString();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+
+    public void ComputerScores(){
+        _computerScore++;
+
+        Debug.Log(_computerScore);
+
+        if(_computerScore == _scoreLimit){
+            this.paddle.Grow();
+            this.ScoreReset();
+        }
+    
+        this.computerScoreText.text = _computerScore.ToString();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+
+    public void ScoreReset(){
+        this._playerScore = 0;
+        this._computerScore = 0;
+        this.computerScoreText.text = _computerScore.ToString();
+        this.playerScoreText.text = _playerScore.ToString();
+    }
+}
---
+++ Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerPaddle : Paddle
+{
+    private Vector2 _direction;
+
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow)){ 
+            _direction = Vector2.up;
+        } else if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow)){
+            _direction = Vector2.down;
+        } else {
+            _direction = Vector2.zero;        
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(_direction.sqrMagnitude != 0){
+            _rigidbody.AddForce(_direction * this.speed);
+        }
+    }
+
+    public void Grow()
+    {
+        this.transform.localScale += new Vector3(0.0f, 0.5f, 0.0f);
+    }
+
+        public void Shrink()
+    {
+        this.transform.localScale += new Vector3(0.0f, -0.5f, 0.0f);
+    }
+}
+ 
